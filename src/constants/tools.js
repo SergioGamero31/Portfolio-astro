166e7140508810ad96fe693460ece4d3d2e39c30
Sergio Gamero <andre25685@gmail.com>
2024-03-21 18:33:55 -0500

    Fix project styles

--- src/constants/tools.js
+++ src/constants/tools.js
@@ -1,74 +1,80 @@
 const TOOLS = {
-    HTML5: {
-        name: "HTML5",
-        src: "/assets/icons/html5.svg",
-        alt: "Icono de HTML5"
-    },
-    CSS: {
-        name: "CSS3",
-        src: "/assets/icons/css.svg",
-        alt: "Icono de CSS3"
-    },
-    JAVASCRIPT: {
-        name: "JavaScript",
-        src: "/assets/icons/javascript.svg",
-        alt: "Icono de JavaScript"
-    },
-    REACT: {
-        name: "React.js",
-        src: "/assets/icons/react.svg",
-        alt: "Icono de React"
-    },
-    VUE: {
-        name: "Vue.js",
-        src: "/assets/icons/vue.svg",
-        alt: "Icono de Vue"
-    },
-    ASTRO: {
-        name: "Astro",
-        src: "/assets/icons/astro.svg",
-        alt: "Icono de Astro"
-    },
-    PINIA: {
-        name: "Pinia",
-        src: "/assets/icons/pinia.svg",
-        alt: "Icono de Pinia"
-    },
-    TAILWIND: {
-        name: "Tailwind CSS",
-        src: "/assets/icons/tailwindcss.svg",
-        alt: "Icono de Tailwind CSS"
-    },
-    NODE: {
-        name: "Node.js",
-        src: "/assets/icons/nodejs.svg",
-        alt: "Icono de Node"
-    },
-    MONGO: {
-        name: "MongoDB",
-        src: "/assets/icons/mongodb.svg",
-        alt: "Icono de MongoDB"
-    },
-    EXPRESS: {
-        name: "Express",
-        src: "/assets/icons/express.svg",
-        alt: "Icono de Express"
-    },
-    GIT: {
-        name: "Git",
-        src: "/assets/icons/git.svg",
-        alt: "Icono de Git"
-    }, 
-    FIGMA: {
-        name: "Figma",
-        src: "/assets/icons/figma.svg",
-        alt: "Icono de Figma"
-    },
-    ADOBEXD: {
-        name: "Adobe XD",
-        src: "/assets/icons/adobe-xd.svg",
-        alt: "Icono de Adobe XD"
-    }
-};
+  HTML5: {
+    name: "HTML5",
+    src: "/assets/icons/html5.svg",
+    alt: "Icono de HTML5"
+  },
+  CSS: {
+    name: "CSS3",
+    src: "/assets/icons/css.svg",
+    alt: "Icono de CSS3"
+  },
+  JAVASCRIPT: {
+    name: "JavaScript",
+    src: "/assets/icons/javascript.svg",
+    alt: "Icono de JavaScript"
+  },
+  REACT: {
+    name: "React.js",
+    src: "/assets/icons/react.svg",
+    alt: "Icono de React",
+    color: "border-[#149eca]"
+  },
+  VUE: {
+    name: "Vue.js",
+    src: "/assets/icons/vue.svg",
+    alt: "Icono de Vue",
+    color: "border-[#42b883]"
+  },
+  ASTRO: {
+    name: "Astro",
+    src: "/assets/icons/astro.svg",
+    alt: "Icono de Astro"
+  },
+  PINIA: {
+    name: "Pinia",
+    src: "/assets/icons/pinia.svg",
+    alt: "Icono de Pinia",
+    color: "border-[#ffd859]"
+  },
+  TAILWIND: {
+    name: "Tailwind CSS",
+    src: "/assets/icons/tailwindcss.svg",
+    alt: "Icono de Tailwind CSS",
+    color: "border-[#38BDF8]"
+  },
+  NODE: {
+    name: "Node.js",
+    src: "/assets/icons/nodejs.svg",
+    alt: "Icono de Node",
+    color: "border-[#84ba64]"
+  },
+  MONGO: {
+    name: "MongoDB",
+    src: "/assets/icons/mongodb.svg",
+    alt: "Icono de MongoDB",
+    color: "border-[#00ED64]"
+  },
+  EXPRESS: {
+    name: "Express",
+    src: "/assets/icons/express.svg",
+    alt: "Icono de Express"
+  },
+  GIT: {
+    name: "Git",
+    src: "/assets/icons/git.svg",
+    alt: "Icono de Git"
+  },
+  FIGMA: {
+    name: "Figma",
+    src: "/assets/icons/figma.svg",
+    alt: "Icono de Figma"
+  },
+  ADOBEXD: {
+    name: "Adobe XD",
+    src: "/assets/icons/adobe-xd.svg",
+    alt: "Icono de Adobe XD"
+  }
+}
 
 export default TOOLS
