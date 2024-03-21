ffa06454cfd6003b5e11192d66e239b161bd8b36
Sergio Gamero <andre25685@gmail.com>
2024-03-21 18:36:16 -0500

    Update tailwind theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'portage': {
+					DEFAULT: '#7A9CFA',
+					300: '#A3C3Fe',
+					500: '#607EF4'
+				}
+			},
+			fontFamily: {
+				'geist': ['Geist Sans']
+			}
+		},
 	},
 	plugins: [],
 }
