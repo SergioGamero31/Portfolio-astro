11063068fb150333a5ed355c03d2caea1bc57fa1
Sergio Gamero <andre25685@gmail.com>
2024-03-22 21:40:19 -0500

    Fix tag colors not showing

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html}'],
+	content: ['./src/**/*.{astro,html,js,jsx}'],
 	theme: {
 		extend: {
 			colors: {
