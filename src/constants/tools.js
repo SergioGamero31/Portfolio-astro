9aecd6538569904f04d7ec97e9024daa6a072260
Andre <andre25685@gmail.com>
2024-07-17 19:41:02 -0500

    Fix missing alt attribute in skills section

--- src/constants/tools.js
+++ src/constants/tools.js
@@ -2,93 +2,94 @@ const TOOLS = {
   HTML5: {
     name: "HTML5",
     src: "/assets/icons/html5.svg",
-    alt: "Icono de HTML5"
+    alt: "Icono de HTML5",
   },
   CSS: {
     name: "CSS3",
     src: "/assets/icons/css.svg",
-    alt: "Icono de CSS3"
+    alt: "Icono de CSS3",
   },
   JAVASCRIPT: {
     name: "JavaScript",
     src: "/assets/icons/javascript.svg",
-    alt: "Icono de JavaScript"
+    alt: "Icono de JavaScript",
   },
   TYPESCRIPT: {
     name: "TypeScript",
-    src: "assets/icons/typescript.svg"
+    src: "assets/icons/typescript.svg",
+    alt: "Icono de TypeScript",
   },
   REACT: {
     name: "React.js",
     src: "/assets/icons/react.svg",
     alt: "Icono de React",
-    color: "border-[#149eca]"
+    color: "border-[#149eca]",
   },
   NEXT: {
     name: "Next.js",
     src: "/assets/icons/next.svg",
-    alt: "Icono de Next"
+    alt: "Icono de Next",
   },
   VUE: {
     name: "Vue.js",
     src: "/assets/icons/vue.svg",
     alt: "Icono de Vue",
-    color: "border-[#42b883]"
+    color: "border-[#42b883]",
   },
   NUXT: {
     name: "Nuxt",
     src: "/assets/icons/nuxt.svg",
-    alt: "Icono de Nuxt"
+    alt: "Icono de Nuxt",
   },
   ASTRO: {
     name: "Astro",
     src: "/assets/icons/astro.svg",
-    alt: "Icono de Astro"
+    alt: "Icono de Astro",
   },
   PINIA: {
     name: "Pinia",
     src: "/assets/icons/pinia.svg",
     alt: "Icono de Pinia",
-    color: "border-[#ffd859]"
+    color: "border-[#ffd859]",
   },
   TAILWIND: {
     name: "Tailwind CSS",
     src: "/assets/icons/tailwindcss.svg",
     alt: "Icono de Tailwind CSS",
-    color: "border-[#38BDF8]"
+    color: "border-[#38BDF8]",
   },
   NODE: {
     name: "Node.js",
     src: "/assets/icons/nodejs.svg",
     alt: "Icono de Node",
-    color: "border-[#84ba64]"
+    color: "border-[#84ba64]",
   },
   MONGO: {
     name: "MongoDB",
     src: "/assets/icons/mongodb.svg",
     alt: "Icono de MongoDB",
-    color: "border-[#00ED64]"
+    color: "border-[#00ED64]",
   },
   EXPRESS: {
     name: "Express",
     src: "/assets/icons/express.svg",
-    alt: "Icono de Express"
+    alt: "Icono de Express",
   },
   GIT: {
     name: "Git",
     src: "/assets/icons/git.svg",
-    alt: "Icono de Git"
+    alt: "Icono de Git",
   },
   FIGMA: {
     name: "Figma",
     src: "/assets/icons/figma.svg",
-    alt: "Icono de Figma"
+    alt: "Icono de Figma",
   },
   ADOBEXD: {
     name: "Adobe XD",
     src: "/assets/icons/adobe-xd.svg",
-    alt: "Icono de Adobe XD"
-  }
-}
+    alt: "Icono de Adobe XD",
+  },
+};
 
-export default TOOLS
+export default TOOLS;
